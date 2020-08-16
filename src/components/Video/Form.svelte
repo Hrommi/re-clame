196e9b394e2f20bc323db44c4d8dac5d6010957c
Stepan <goodhrommi@gmail.com>
2020-08-16 15:31:19 +0300

    add validation for url

--- src/components/Video/Form.svelte
+++ src/components/Video/Form.svelte
@@ -1,31 +1,81 @@
 <script>
   import { onDestroy, getContext } from 'svelte';
+  import * as yup from 'yup';
 
   export let key;
   let { url } = getContext(key);
 
-  let value = 'https://sveltejs.github.io/assets/caminandes-llamigos.mp4';
+  const SUPPORTED_EXTENSIONS = [
+    'WEBM',
+    'OGG',
+    'MP4',
+    'AVI',
+  ];
+
+  let value = '';
+  let valid = true;
+  let errorMessage = null;
+
+  const validateUrl = (currentUrl) => {
+    const urlSchema = yup
+      .string()
+      .url('Link must be a valid URL')
+      .required('Link is a required field')
+      .test(
+        'fileType',
+        'Unsupported file extension',
+        (currentValue) => {
+          const extension = currentValue.substring(
+            currentValue.lastIndexOf('.') + 1,
+            currentValue.length
+          );
+          return SUPPORTED_EXTENSIONS.includes(extension.toUpperCase());
+        }
+      );
+
+    try {
+      urlSchema.validateSync(currentUrl);
+      return null;
+    } catch (e) {
+      return e.message;
+    }
+  };
 
   const handleSubmit = () => {
-    $url = value;
-  }
+    const error = validateUrl(value);
+    if (error) {
+      valid = false;
+      errorMessage = error;
+    } else {
+      valid = true;
+      errorMessage = null;
+      $url = value;
+    }
+  };
 
   const unsubscribe = url.subscribe((currentUrl) => {
     value = currentUrl;
-  })
+  });
 
   onDestroy(unsubscribe);
 </script>
 
-<form class="mb-5" on:submit|preventDefault={handleSubmit}>
-  <div class="field has-addons">
-    <div class="control is-expanded">
-      <input bind:value class="input" type="text" placeholder="url">
+<form class='mb-5' on:submit|preventDefault={handleSubmit}>
+  <div class='field has-addons'>
+    <div class='control is-expanded'>
+      <input
+        class='input'
+        class:is-danger={!valid}
+        bind:value
+        type='text'
+        placeholder='link'
+      >
+      {#if errorMessage}
+        <p class='help is-danger'>{errorMessage}</p>
+      {/if}
     </div>
-    <div class="control">
-      <button class="button is-info">
-        load video
-      </button>
+    <div class='control'>
+      <button class='button is-info'>load video</button>
     </div>
   </div>
 </form>
